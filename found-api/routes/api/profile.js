9e9208d8c064fc1b237cf8a802878bf64de95aa7
picongzhi <picongzhi@gmail.com>
2019-10-02 21:24:46 +0800

    feat(profile): add prifile add api

--- found-api/routes/api/profile.js
+++ found-api/routes/api/profile.js
@@ -5,4 +5,42 @@ const Profile = require('../../models/profile');
 
 const router = express.Router();
 
+router.post('/add', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const profileFields = {};
+    if (req.body.type) {
+        profileFields.type = req.body.type;
+    }
+
+    if (req.body.desc) {
+        profileFields.desc = req.body.desc;
+    }
+
+    if (req.body.income) {
+        profileFields.income = req.body.income;
+    }
+
+    if (req.body.expand) {
+        profileFields.expand = req.body.expand;
+    }
+
+    if (req.body.cash) {
+        profileFields.cash = req.body.cash;
+    }
+
+    if (req.body.remark) {
+        profileFields.remark = req.body.remark;
+    }
+
+    new Profile(profileFields).save()
+        .then(profile => {
+            if (profile) {
+                return res.json(profile);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+});
+
 module.exports = router;
