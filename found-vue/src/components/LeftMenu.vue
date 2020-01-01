744063c9353760c96158cb6b90aeeab371bc023c
picongzhi <picongzhi@gmail.com>
2020-01-01 18:06:49 +0800

    feat(menu): add left menu

---
+++ found-vue/src/components/LeftMenu.vue
@@ -0,0 +1,104 @@
+<template>
+  <el-row class="menu-page">
+    <el-col>
+      <el-menu
+        default-active="2"
+        background-color="#324057"
+        text-color="#fff"
+        active-text-color="#409efff"
+      >
+        <router-link to="/home">
+          <el-menu-item index="0">
+            <i class="fa fa-margin fa-server"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+        </router-link>
+
+        <template v-for="item in items">
+          <el-submenu v-if="item.children" :index="item.path" :key="item.path">
+            <template slot="title">
+              <i :class="'fa fa-margin ' + item.icon"></i>
+              <span slot="title">{{ item.name }}</span>
+            </template>
+            <router-link
+              v-for="(subItem, subIndex) in item.children"
+              :to="subItem.path"
+              :key="subIndex"
+            >
+              <el-menu-item :index="subItem.path">
+                <span slot="title">{{ subItem.name }}</span>
+              </el-menu-item>
+            </router-link>
+          </el-submenu>
+        </template>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'LeftMenu',
+  data () {
+    return {
+      items: [
+        {
+          icon: 'fa-money',
+          name: '资金管理',
+          path: 'fund',
+          children: [
+            {
+              path: 'foundList',
+              name: '资金流水'
+            }
+          ]
+        },
+        {
+          icon: 'fa-asterisk',
+          name: '信息管理',
+          path: 'info',
+          children: [
+            {
+              path: 'info',
+              name: '个人信息'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu-page {
+  position: fixed;
+  top: 71px;
+  left: 0;
+  min-height: 100%;
+  background-color: #324057;
+  z-index: 99;
+}
+
+.el-menu {
+  border: none;
+  width: 35px;
+}
+
+.fa-margin {
+  margin-right: 5px;
+}
+
+.el-submenu .el-menu-item {
+  min-width: 180px;
+}
+
+.el-menu:not(.el-menu--collapse) {
+  width: 180px;
+  min-height: 400px;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
--- found-vue/src/views/Index.vue
+++ found-vue/src/views/Index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="index">
     <head-nav></head-nav>
+    <left-menu></left-menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HeadNav from "@/components/HeadNav"
+import HeadNav from '@/components/HeadNav'
+import LeftMenu from '@/components/LeftMenu'
 
 export default {
-  name: "Index",
-  components: { HeadNav }
+  name: 'Index',
+  components: { HeadNav, LeftMenu }
 }
 </script>
 
